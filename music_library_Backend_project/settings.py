8e2f875492d5b48c9dde5d10968d84ccfa636533
C-Skala <cjskala@gmail.com>
2022-08-24 21:58:31 -0400

    made app for database

--- music_library_Backend_project/settings.py
+++ music_library_Backend_project/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!43i7&vvl@8ifw&a2$-hma#kw*bs1_jd987(!7+4qth8nz^ta'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -73,12 +69,7 @@ WSGI_APPLICATION = 'music_library_Backend_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -121,3 +112,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from music_library_Backend_project.local_settings import *
+except ImportError:
+    pass
