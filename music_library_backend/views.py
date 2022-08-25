af1570c4d18b74ca0a9f86bbae4bfaa134c1135c
C-Skala <cjskala@gmail.com>
2022-08-24 22:41:43 -0400

    added views and updated urls

--- music_library_backend/views.py
+++ music_library_backend/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django import views
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Song
+from .serializers import SongSeralizer
+from music_library_backend import serializers
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def song_list(request):
+   
+   
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSeralizer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSeralizer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+            
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSeralizer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSeralizer(song, data = request.data)
+        serializer.is_valid(raise_exception= True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status= status.HTTP_204_NO_CONTENT)
